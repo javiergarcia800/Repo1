5e9281d2d2caafa2fadf1dca8256ba7a2832beba
javierislas800 <javierislas@gmail.com>
2012-07-20 23:31:28 -0500

    Se elimina el borde de la ventana y se centra la ventana.

--- 8Reinas/src/mx/com/reinas/gui/ReinasGUI.scala
+++ 8Reinas/src/mx/com/reinas/gui/ReinasGUI.scala
@@ -1,10 +1,10 @@
 package mx.com.reinas.gui
 
 import scala.swing._
-import scala.swing.Button
+import scala.swing.RichWindow.Undecorated
 import scala.swing.Panel
 
-import java.awt.{Color, Dimension}
+import java.awt.{Color, Dimension, Toolkit}
 import java.awt.geom._
 import javax.swing._
 
@@ -12,13 +12,23 @@ import javax.swing._
 
 object ReinasGUI extends SimpleSwingApplication {
   
-  def top = new MainFrame{
+  val tableroAjedrez = new MyCanvasPanel
+  
+  val boundsCenter = getRectangleCenter(tableroAjedrez.getDimension())
+  
+  def top = new MainFrame with Undecorated{
     title = "8 Reinas"
-    contents = new MyCanvasPanel
-    size = new Dimension(501,523)
-    /*contents += new Button(""){
-      text = "Click me"
-    } */ 
+    bounds = boundsCenter
+    contents = tableroAjedrez
+    size = new Dimension(tableroAjedrez.getDimension().width, tableroAjedrez.getDimension().height)
+    resizable = false
+  }
+  
+  def getRectangleCenter(windowDimension:Dimension) = {
+    var screen = Toolkit.getDefaultToolkit().getScreenSize()
+    var X = (screen.width / 2) - (windowDimension.getWidth() / 2); // Center horizontally.
+    var Y = (screen.height / 2) - (windowDimension.getHeight() / 2); // Center vertically.
+    new Rectangle(X.intValue(), Y.intValue(), windowDimension.getWidth().intValue(), windowDimension.getHeight().intValue())
   }
   
 }
@@ -44,15 +54,42 @@ class MyCanvasPanel extends Panel{
   val ANCHO_MARCO = 4
   
   override def paint(g:Graphics2D){
-    
-    // Dibuja fondo
+    this.dibujaFondo(g)
+    this.dibujaBorde(g)
+    this.dibujaCuadros(g)
+  }
+  
+  /**
+   * Dibuja el fondo del tablero.
+   */
+  def dibujaFondo(g:Graphics2D) {
     g.setColor(BACKGROUND_COLOR)
     g.fillRect(0,0,(NUMERO_REINAS*TAMANO_CASILLAS+(ESPACIO_LIBRE*2)+(ANCHO_MARCO*2)),(NUMERO_REINAS*TAMANO_CASILLAS+(ESPACIO_LIBRE)+(ANCHO_MARCO*2)+ 3))
-    
-    // Dibuja el borde
-    //g.setColor(BORDER_COLOR)
-    this.dibujaBorde(g, ESPACIO_LIBRE, ESPACIO_LIBRE, (NUMERO_REINAS*TAMANO_CASILLAS+(ANCHO_MARCO*2)), (NUMERO_REINAS*TAMANO_CASILLAS+(ANCHO_MARCO*2)), ANCHO_MARCO)
-    
+  }
+  
+  /**
+   * Dibuja el borde del tablero.
+   */
+  def dibujaBorde(g:Graphics2D) {
+    var x = ESPACIO_LIBRE
+    var y = ESPACIO_LIBRE
+    var width = (NUMERO_REINAS*TAMANO_CASILLAS+(ANCHO_MARCO*2))
+    var height = (NUMERO_REINAS*TAMANO_CASILLAS+(ANCHO_MARCO*2))
+    // El borde se construye con 5 lineas.
+    g.setColor(COLOR_BORDE_EXTERNO_2)
+    g.draw(new Rectangle2D.Double(x + 0, y + 0, width - 1, height - 1))
+    g.setColor(COLOR_BORDE_EXTERNO_1)
+    g.draw(new Rectangle2D.Double(x + 1, y + 1, width - 2 - 1, height - 2 - 1))
+    g.setColor(COLOR_BORDE_EXTERNO_0)
+    g.draw(new Rectangle2D.Double(x + 2, y + 2, width - 4 - 1, height - 4 - 1))
+    g.setColor(COLOR_BORDE_EXTERNO_1)
+    g.draw(new Rectangle2D.Double(x + 3, y + 3, width - 6 - 1, height - 6 - 1))
+  }
+  
+  /**
+   * Dibuja el tablero completo.
+   */
+  def dibujaCuadros(g:Graphics2D) {
     var INIT_X = ESPACIO_LIBRE + ANCHO_MARCO;
     var INIT_Y = ESPACIO_LIBRE + ANCHO_MARCO;
     
@@ -73,35 +110,21 @@ class MyCanvasPanel extends Panel{
       y += TAMANO_CASILLAS
       x = INIT_X
     }
-    
-    
   }
   
+  /**
+   * Dibuja una casilla el tablero, blanca o negra.
+   */
   def dibujaCasilla(g:Graphics2D, x:Int, y:Int, color:Color) {
     g.setColor(color)
     g.fill(new Rectangle2D.Double(x, y, TAMANO_CASILLAS, TAMANO_CASILLAS))
   }
   
   /**
-   * Dibuja el borde del tablero.
+   * Regresa las dimensiones de la ventana del tablero.
    */
-  def dibujaBorde(g:Graphics2D, x:Int, y:Int, width:Int, height:Int, widthLinea:Int) {
-    // El borde se construye con 6 lineas.
-    g.setColor(COLOR_BORDE_EXTERNO_2)
-    g.draw(new Rectangle2D.Double(x + 0, y + 0, width - 1, height - 1))
-    g.setColor(COLOR_BORDE_EXTERNO_1)
-    g.draw(new Rectangle2D.Double(x + 1, y + 1, width - 2 - 1, height - 2 - 1))
-    g.setColor(COLOR_BORDE_EXTERNO_0)
-    g.draw(new Rectangle2D.Double(x + 2, y + 2, width - 4 - 1, height - 4 - 1))
-    g.setColor(COLOR_BORDE_EXTERNO_1)
-    g.draw(new Rectangle2D.Double(x + 3, y + 3, width - 6 - 1, height - 6 - 1))
-    //g.setColor(COLOR_BORDE_EXTERNO_2)
-    //g.draw(new Rectangle2D.Double(x + 4, y + 4, width - 8 - 1, height - 8 - 1))
-    
-    /*for (i<- 0 until widthLinea ){
-      g.draw((new Rectangle2D.Double(x + i, y + i, width-(2*i)-1, height-(2*i)-1)))
-    }*/
-    
-  }
+  def getDimension() =
+    new Dimension(NUMERO_REINAS * TAMANO_CASILLAS+ (ESPACIO_LIBRE*2) + (ANCHO_MARCO*2),
+                  NUMERO_REINAS * TAMANO_CASILLAS+ (ESPACIO_LIBRE*2) + (ANCHO_MARCO*2))
 
 }
